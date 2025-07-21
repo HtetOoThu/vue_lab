faf367c3dd1a4e55066a7b87ee85868982325af4
Htet Oo Thu <htetoothu_htet@cmu.ac.th>
2025-07-21 14:24:10 +0700

    commit 5

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -5,5 +5,19 @@
 </script>
 
 <template>
-  <div class="greetings"></div> 
+  <div class="event-class"></div> 
 </template>
+
+<style scoped>
+.event-class{
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+.event-class:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px rgba(0, 0, 0, 0.2);
+}
+</style>
