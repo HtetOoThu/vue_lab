9ed83db86be1feed9e850b472a80172a8aded35c
Htet Oo Thu <htetoothu_htet@cmu.ac.th>
2025-07-25 09:43:46 +0700

    commit lab3 1

--- 331-intro-to-vite/src/components/EventCard.vue
+++ 331-intro-to-vite/src/components/EventCard.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import {ref} from 'vue'
 import type { Event } from '@/types'
 import EventCard2 from './icons/EventCard2.vue';
 
@@ -20,6 +19,7 @@ defineProps<{
 </script>
 
 <template>
+  <RouterLink class="event-link":to="{ name: 'event-detail-view', params: { id: event.id } }">
   <div class="event-class">
     <div class="event-card">
       <h2>{{ event.title }}</h2>
@@ -27,6 +27,7 @@ defineProps<{
       <EventCard2 :event="event" />
     </div>
   </div> 
+  </RouterLink>  
 </template>
 
 <style scoped>
@@ -40,5 +41,9 @@ defineProps<{
 .event-class:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px rgba(0, 0, 0, 0.2);
+}
+.event-link{
+  text-decoration: none;
+  color: #2c3e50
 }
 </style>
