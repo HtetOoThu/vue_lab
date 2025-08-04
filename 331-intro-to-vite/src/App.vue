19532dfc743ddf1fb46594154b0814d92e5e2b14
Htet Oo Thu <htetoothu_htet@cmu.ac.th>
2025-08-04 14:39:54 +0700

    commit lab 5.4

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -22,7 +22,7 @@ watch(pageSize, (newSize) => {
 </script>
 
 <template>
-  <div id="layout">
+  <div class="text-center font-sans text-gray-700 antialias">
     <header>
       <div id="flashMessage" v-if="messages">
         <h4>{{ messages }}</h4>
@@ -44,14 +44,6 @@ watch(pageSize, (newSize) => {
 </template>
 
 <style>
-#layout{
-  font-family: Avenir, Arial, Helvetica, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
 nav {
   padding: 30px;
 }
