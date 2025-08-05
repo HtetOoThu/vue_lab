1b561c0692cf97dd1af6c18772e062715a52a07e
Htet Oo Thu <htetoothu_htet@cmu.ac.th>
2025-08-05 13:46:18 +0700

    commit lab 5.10

--- 331-intro-to-vite/src/App.vue
+++ 331-intro-to-vite/src/App.vue
@@ -24,7 +24,7 @@ watch(pageSize, (newSize) => {
 <template>
   <div class="text-center font-sans text-gray-700 antialias">
     <header>
-      <div id="flashMessage" v-if="messages">
+      <div id="flashMessage" class="animate-fade" v-if="messages">
         <h4>{{ messages }}</h4>
       </div>
       <div class="wrapper">
@@ -34,7 +34,7 @@ watch(pageSize, (newSize) => {
 
           <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" 
             :to="{ name: 'about' }">About</RouterLink>
-            
+
           <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" 
             :to="{ name: 'student-list-view' }">Students</RouterLink>
         </nav>
@@ -47,35 +47,3 @@ watch(pageSize, (newSize) => {
   </div>
   <RouterView />
 </template>
-
-<style>
-nav {
-  padding: 30px;
-}
-
-nav a{
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-h2{
-  font-size: 20px;
-}
-
-@keyframes yellofade {
-  from {
-    background-color: yellow;
-  }
-  to {
-    background-color: transparent;
-  }
-}
-
-#flashMessage {
-  animation: yellofade 3s ease-in-out;
-}
-</style>
